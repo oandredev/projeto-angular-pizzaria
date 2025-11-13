8ac1d2a26517adae405637009141f79fb7f65bc2
André Rodrigues <oandredev@gmail.com>
2025-11-12 21:55:33 -0300

    feat: add all the basic components

--- projeto-angular-pizzaria/src/app/app.config.ts
+++ projeto-angular-pizzaria/src/app/app.config.ts
@@ -1,12 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(), // <- Adicione isso aqui
+  ],
 };
---
+++ projeto-angular-pizzaria/src/app/pages/offer-details/offer-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-details',
+  imports: [],
+  templateUrl: './offer-details.html',
+  styleUrl: './offer-details.css',
+})
+export class OfferDetails {
+
+}
