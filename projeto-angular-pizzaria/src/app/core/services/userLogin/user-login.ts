377ac6d4c42449936b040d2c21e7e7854fbf59b7
André Rodrigues <oandredev@gmail.com>
2025-11-18 21:18:37 -0300

    feat: Integrate login with json-server

--- projeto-angular-pizzaria/src/app/core/services/userLogin/user-login.ts
+++ projeto-angular-pizzaria/src/app/core/services/userLogin/user-login.ts
@@ -7,32 +7,24 @@ import { User } from '../../types/types';
   providedIn: 'root',
 })
 export class UserLoginService {
-  private currentUser: User | null = null;
+  private registerAPI = `http://localhost:3000/users`;
+  private loginAPI = `http://localhost:3000/loggedUser`;
 
   constructor(private http: HttpClient) {}
 
   tryLogin(email: string, password: string): Observable<User | null> {
-    return this.http.get<User[]>('http://localhost:3000/users').pipe(
+    return this.http.get<User[]>(this.registerAPI).pipe(
       map((users) => {
         return users.find((u) => u.email === email && (u as any).password === password) || null;
       })
     );
   }
 
-  logout() {
-    this.currentUser = null;
-  }
-
+  // Use PUT instead of POST to always keep only one item saved.
   setUser(user: User) {
-    this.currentUser = user;
-    console.log('User Logged: ' + user.name);
-  }
-
-  getUser(): User | null {
-    return this.currentUser;
-  }
-
-  isLogged(): boolean {
-    return this.currentUser !== null;
+    return this.http.put(`${this.loginAPI}`, {
+      id: '1',
+      user,
+    });
   }
 }
