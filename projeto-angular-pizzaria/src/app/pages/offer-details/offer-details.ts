40eff740559911334a474bdec95e013c1973e679
André Rodrigues <oandredev@gmail.com>
2025-11-21 13:42:59 -0300

    feat: add summary integration

--- projeto-angular-pizzaria/src/app/pages/offer-details/offer-details.ts
+++ projeto-angular-pizzaria/src/app/pages/offer-details/offer-details.ts
@@ -14,10 +14,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './offer-details.css',
 })
 export class OfferDetails implements OnInit {
-  private offer: Offer | null = null;
-  private baseIngredients: string[] = [];
-  pizzaImage: String = '';
-  customizations: CustomizationOptions[] = [];
+  offer: Offer | null = null;
+  baseIngredients: string = '';
+  customizationsAvailable: CustomizationOptions[] = [];
+  customizationsSelecteds: CustomizationOptions[] = [];
+
+  subtotal: String = '0.00';
 
   constructor(
     private route: ActivatedRoute,
@@ -30,7 +32,6 @@ export class OfferDetails implements OnInit {
     const id = String(this.route.snapshot.paramMap.get('id'));
     this.loadOffer(id);
   }
-
   // Make two requests: the first loads the offer and the second loads the ingredients using an array from the offer.
   loadOffer(id: string) {
     this.offersService
@@ -44,6 +45,7 @@ export class OfferDetails implements OnInit {
             return of([]);
           }
 
+          // Organize default ingredients
           const ingredientObservables = (this.offer.ingredients as string[]).map((ingredientId) =>
             this.offersService.getIngredientById(ingredientId)
           );
@@ -54,7 +56,11 @@ export class OfferDetails implements OnInit {
       .subscribe({
         next: (ingredients: any[]) => {
           if (this.offer) {
-            this.baseIngredients = ingredients.filter((i) => i && i.name).map((i) => i.name);
+            this.baseIngredients =
+              ingredients
+                .filter((i) => i && i.name)
+                .map((i) => i.name)
+                .join(', ') + '.';
             this.loadCustomization();
           }
         },
@@ -71,11 +77,11 @@ export class OfferDetails implements OnInit {
 
     this.offersService.getCustomizationsOfferObjects(customizationIds).subscribe({
       next: (customizations) => {
-        this.customizations = customizationIds
+        this.customizationsAvailable = customizationIds
           .map((id) => customizations.find((c) => String(c.id) === id))
           .filter(Boolean) as CustomizationOptions[];
 
-        this.updateHTMLElements();
+        this.calculateSubtotal();
       },
       error: (err) => {
         console.error('Erro ao carregar customizações', err);
@@ -84,38 +90,75 @@ export class OfferDetails implements OnInit {
     });
   }
 
-  updateHTMLElements() {
-    const pizzaTitleElement = document.getElementById('pizzaTitle');
-    const pizzaImageElement = document.getElementById('pizzaPreview');
-    const pizzaDescriptionElement = document.getElementById('pizzaDescription');
-    const ingredientesElement = document.getElementById('ingredients');
-    const baseValueElement = document.getElementById('baseValue');
-
-    if (
-      pizzaTitleElement &&
-      pizzaImageElement &&
-      pizzaDescriptionElement &&
-      ingredientesElement &&
-      baseValueElement
-    ) {
-      pizzaTitleElement.textContent = this.offer?.name ?? '';
-      this.pizzaImage = this.offer?.images[0] ?? '';
-      pizzaDescriptionElement.textContent = this.offer?.description ?? '';
-      ingredientesElement.textContent = this.baseIngredients.join(', ') + '.';
-      baseValueElement.textContent = 'R$: ' + this.offer?.priceBase;
-    } else {
-      this.forceRedirection();
-    }
-  }
-
   getCustomizationsByType(type: string): CustomizationOptions[] {
-    return this.customizations.filter((c) => c.type === type);
+    return this.customizationsAvailable.filter((c) => c.type === type);
   }
 
   // Used when something fails (offer unavailable or elements of HTML not found)
   forceRedirection() {
     alert('Algo falhou durante o carregamento. Voltando...');
     this.router.navigate(['/offers']);
+  }
+
+  refreshPreviewOfCustomization(opt: CustomizationOptions) {
+    // Refresh Array
+    const isExtraOrSem = opt.type === 'EXTRA' || opt.type === 'SEM';
+    const isSelected = this.customizationsSelecteds.some((item) => item.id === opt.id);
+
+    if (isSelected) {
+      this.customizationsSelecteds = this.customizationsSelecteds.filter(
+        (item) => item.id !== opt.id
+      );
+    } else {
+      if (!isExtraOrSem) {
+        this.customizationsSelecteds = this.customizationsSelecteds.filter(
+          (item) => item.type !== opt.type
+        );
+      }
+
+      this.customizationsSelecteds.push(opt);
+    }
+    this.calculateSubtotal();
+  }
+
+  calculateSubtotal() {
+    const base = parseFloat(this.offer?.priceBase ?? '0.00');
+    const extras = this.customizationsSelecteds
+      .map((opt) => parseFloat(opt.value))
+      .reduce((acc, val) => acc + val, 0);
+
+    const total = base + extras;
+
+    this.subtotal = total.toFixed(2);
+  }
+
+  removeCustomization(item: CustomizationOptions) {
+    this.customizationsSelecteds = this.customizationsSelecteds.filter(
+      (itemRef) => itemRef.id !== item.id
+    );
+
+    this.calculateSubtotal();
+  }
+
+  // Called by checked by each checkbox/radiobox
+  isSelected(opt: CustomizationOptions): boolean {
+    return this.customizationsSelecteds.some((item) => item.id === opt.id);
+  }
+
+  get isFormValid(): boolean {
+    // pegar os tipos obrigatórios que existem na pizza
+    const mandatoryTypes = Array.from(
+      new Set(
+        this.customizationsAvailable
+          .filter((opt) => opt.type !== 'EXTRA' && opt.type !== 'SEM')
+          .map((opt) => opt.type)
+      )
+    );
+
+    // validar se cada tipo obrigatório tem pelo menos uma seleção
+    return mandatoryTypes.every((type) =>
+      this.customizationsSelecteds.some((sel) => sel.type === type)
+    );
   }
 
   addToCart() {
