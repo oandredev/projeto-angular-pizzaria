cd855922cc06bb1c1ba82e7b7c46f5130820adb4
André Rodrigues <oandredev@gmail.com>
2025-11-21 00:01:51 -0300

    feat: Enhance offer details with dynamic customization options

--- projeto-angular-pizzaria/src/app/core/services/offers/offers.ts
+++ projeto-angular-pizzaria/src/app/core/services/offers/offers.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
-import { Offer, Ingredient } from '../../types/types';
+import { Offer, Ingredient, CustomizationOptions } from '../../types/types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OffersService {
+  // APIs
   private offerAPI = `http://localhost:3000/offers`;
   private ingredientAPI = `http://localhost:3000/ingredients`;
+  private customizationOptionsAPI = `http://localhost:3000/customizationOptions`;
 
   constructor(private http: HttpClient) {}
 
-  getOfferData(offerId: String) {
+  getOfferData(offerId: string) {
     return this.http.get<Offer[]>(this.offerAPI).pipe(
       map((offers) => {
         return offers.find((o) => o.id === offerId) || null;
@@ -22,5 +24,18 @@ export class OffersService {
 
   getIngredientById(id: string): Observable<Ingredient | null> {
     return this.http.get<Ingredient>(`${this.ingredientAPI}/${id}`);
+  }
+
+  getCustomizationsOfferObjects(ids: string[]): Observable<CustomizationOptions[]> {
+    return this.http
+      .get<CustomizationOptions[]>(this.customizationOptionsAPI)
+      .pipe(
+        map(
+          (allCustomizations) =>
+            ids
+              .map((id) => allCustomizations.find((c) => String(c.id ?? c.id) === String(id)))
+              .filter(Boolean) as CustomizationOptions[]
+        )
+      );
   }
 }
