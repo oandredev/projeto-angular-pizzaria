e338ed6880a78b6a785686aa97ffa3f5fbcc69df
souzafe13 <souzafe13@gmail.com>
2025-11-22 19:33:13 -0300

    feat: Implementação das páginas Offers e About Us e unificação do estilo dos cards

--- projeto-angular-pizzaria/src/app/pages/about-us/about-us.ts
+++ projeto-angular-pizzaria/src/app/pages/about-us/about-us.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [CommonModule, RouterModule], 
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
 export class AboutUs {
 
+  constructor() { }
 }
