a04bcbb0953189f42ffa6c5df8f48768bca41e2e
André Rodrigues <oandredev@gmail.com>
2025-11-22 20:04:33 -0300

    test: payment and qr code screens

---
+++ projeto-angular-pizzaria/src/app/pages/payment-qrcode/payment-qrcode.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-qrcode',
+  imports: [],
+  templateUrl: './payment-qrcode.html',
+  styleUrl: './payment-qrcode.css',
+})
+export class PaymentQRCode {
+
+}
