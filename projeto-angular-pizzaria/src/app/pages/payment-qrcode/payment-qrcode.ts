4991120f636d8c7cdf264917072888047c7ce5ad
André Rodrigues <oandredev@gmail.com>
2025-11-23 15:55:09 -0300

    feat: add integration between cart, qrCode/purchaseConfirmation screens

--- projeto-angular-pizzaria/src/app/pages/payment-qrcode/payment-qrcode.ts
+++ projeto-angular-pizzaria/src/app/pages/payment-qrcode/payment-qrcode.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-payment-qrcode',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './payment-qrcode.html',
   styleUrl: './payment-qrcode.css',
 })
 export class PaymentQRCode {
-
+  constructor(private router: Router) {}
 }
